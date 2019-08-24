d8c2269ff0f22797d63603b8db4419253ec90edb
OliverMeredith <mail@olivermeredith.com>
2019-08-24 13:12:21 +0100

    re styled, add svg umbrella

--- public/main.js
+++ public/main.js
@@ -5,9 +5,6 @@ mapboxgl.accessToken =
     'pk.eyJ1Ijoib2FqbWVyZWRpdGgyMyIsImEiOiJjanZzM3ZxYW4yc25xNDRtZ25rcjJvbmRyIn0.6vRe5vbyV0NJ83oOVWdnMQ';
 
 
-
-
-
 if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(async position => {
 
@@ -17,46 +14,77 @@ if ('geolocation' in navigator) {
 
         const response = await fetch(api_url);
         const weather = await response.json();
-        const forecast = weather.hourly.data
-        console.log(forecast)
-        for (let i = 0; i < 12; i++) {
-            chanceOfRain += forecast[i].precipProbability;
-            temp.push(forecast[i].apparentTemperature)
-        }
-        console.log(temp)
+        const forecast = weather.hourly.data.slice(0, 12); // return first 12 hours of forecast
 
-        let totalChanceOfRain = chanceOfRain / 12;
+        const chanceOfRain = await returnProcessedWeather.rain(forecast) // 
 
-        const reducer = (accumulator, currentValue) => accumulator + currentValue;
-        let totalTemp = (((temp.reduce(reducer) / 12) - 32) / 1.8).toFixed(2);
-
-
-        if (totalChanceOfRain > 5) {
-            var rainOrnot = "Yep"
-        } else {
-            var rainOrnot = "Nope"
-        }
-
-        let rainMessage = `<h2>${rainOrnot}<h2><h3>there's a ${totalChanceOfRain * 100}% chance of rain over the next 12 hours</h3>`
-        document.getElementById('rain').innerHTML = rainMessage
-
-        if (totalTemp > 20) {
-            var jacketOrNot = "Probably not"
-        } else {
-            var jacketOrNot = "I should think so"
-        }
-        let jackMessage = `<h2>${jacketOrNot}<h2><h3>it's averaging ${totalTemp}° over the next 12 hours</h3>`
-        document.getElementById('jacket').innerHTML = jackMessage;
-
-
-        let map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/oajmeredith23/cjw0tt4531mit1cnvqvsu9x6a',
-            center: [lon, lat],
-            zoom: 9
-        });
-
+        await coat(chanceOfRain, showMap(lat, lon))
 
     })
 
 }
+
+function coat(chanceOfRain, callback) {
+    chanceOfRain = 70
+    if (chanceOfRain > 60) {
+        var rainOrnot = "Yep"
+    } else if (chanceOfRain > 30) {
+        var rainOrnot = "Mmm, maybe"
+    } else {
+        var rainOrnot = "Nope"
+    }
+
+    let rainMessage = `
+        <h2>${rainOrnot}<h2>
+        <h3>${chanceOfRain}% chance of rain</h3>
+        <p>over the next 12 hours</p>
+    `
+
+    const target = document.querySelector('#answer')
+    const umbrella = document.querySelector('#umbrella')
+
+    const strokeLength = umbrella.getTotalLength()
+    const newStrokeLength = Math.floor((strokeLength - (chanceOfRain / 100) * strokeLength)) - 1;
+
+    target.innerHTML = rainMessage;
+
+    setTimeout(() => {
+        target.classList.add('in-view')
+        umbrella.style.strokeDashoffset = 0
+        setTimeout(() => { umbrella.style.strokeDashoffset = newStrokeLength }, 0)
+        // umbrella.style.strokeDashoffset = newStrokeLength
+        umbrella.classList.remove('is-loading')
+
+        callback()
+    }, 0)
+
+}
+
+const returnProcessedWeather = {
+
+    rain: (weather) => {
+
+        for (let i = 0; i < 12; i++) {
+            chanceOfRain += weather[i].precipProbability;
+        }
+
+        return totalChanceOfRain = (chanceOfRain / 12).toFixed(2) * 100;
+    },
+
+    temp: (weather) => {
+
+    }
+}
+
+function showMap(lat, lon) {
+    let map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/oajmeredith23/cjw0tt4531mit1cnvqvsu9x6a',
+        center: [lon, lat],
+        zoom: 12
+    });
+
+
+}
+
+
